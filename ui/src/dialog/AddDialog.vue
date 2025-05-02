031640bb7f514de9b12eb7676483df5a0a4ae6a2
HapiFive <1143910315@qq.com>
2025-05-02 13:58:38 +0800

    录制操作ui

--- ui/src/dialog/AddDialog.vue
+++ ui/src/dialog/AddDialog.vue
@@ -5,7 +5,24 @@
       <div class="s1"></div>
       <div class="radius"><el-text type="primary" size="small">F1</el-text></div>
     </div>
-    <span>This is a message</span>
+    <el-table
+      ref="singleTableRef"
+      :data="tableData"
+      highlight-current-row
+      style="width: 100%"
+      @current-change="handleCurrentChange"
+    >
+      <el-table-column property="operation" label="操作" />
+      <el-table-column label="操作">
+        <template #default="scope">
+          <el-button size="small" @click="handleEdit(scope.$index, scope.row)">播放</el-button>
+          <el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
     <template #footer>
       <div class="dialog-footer">
         <el-button @click="dialogVisible = false">Cancel</el-button>
@@ -27,6 +44,7 @@
 </style>
 <script lang="ts" setup>
 import { ElMessageBox } from 'element-plus'
+import { ref } from 'vue'
 
 const dialogVisible = defineModel<boolean>()
 
@@ -39,4 +57,20 @@ const handleClose = (done: () => void) => {
       // catch error
     })
 }
+interface macro {
+  date: string
+  name: string
+  address: string
+}
+const currentRow = ref()
+const handleCurrentChange = (val: macro | undefined) => {
+  currentRow.value = val
+}
+const handleEdit = (index: number, row: macro) => {
+  console.log(index, row)
+}
+const handleDelete = (index: number, row: macro) => {
+  console.log(index, row)
+}
+const tableData: macro[] = []
 </script>
